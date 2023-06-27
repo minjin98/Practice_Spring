671a402a5e5409afc0ddbf495a7941d60b876f15
minjin98 <ebs5296@naver.com>
2023-06-27 17:51:52 +0900

    [Issue] DB 연동 및 이슈 정보 받아오기

--- Issue/src/main/java/spring/dao/MainForIssueDao.java
+++ Issue/src/main/java/spring/dao/MainForIssueDao.java
@@ -8,25 +8,26 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 
 public class MainForIssueDao {
-	private static IssueDao IssueDao;
+	//ivate static IssueDao IssueDao;
 
 	public static void main(String[] args) {
+		System.out.println("MainforIssueDao");
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
 
-		IssueDao = ctx.getBean(IssueDao.class);
-
-		selectAll();
+		IssueDao issueDao= ctx.getBean(IssueDao.class);
+ 
+		selectAll(issueDao);
 
 		ctx.close();
 	}
 
-	private static void selectAll() {
+	private static void selectAll(IssueDao issueDao) {
 		System.out.println("----- selectAll");
-		int total = IssueDao.count();
+		int total = issueDao.count();
 		System.out.println("전체 데이터: " + total);
-		List<Issue> Issues = IssueDao.selectAll();
+		List<Issue> Issues = issueDao.selectAll();
 		for (Issue m : Issues) {
-			System.out.println(m.getIssueNo() + ":" + m.getIssueName() + ":" + m.getIssueInfo() + ":" +m.getTimestamp());
+			System.out.println(m.getIssueNo() + ":" + m.getIssueName() + ":" + m.getIssueInfo() + m.getTimestamp());
 		}
 	}
 
